2ccc32c401e24cec0e1c5dee8c234acecb684700
Takashi Watanabe <yamanokama@gmail.com>
2012-11-15 22:18:26 +0900

    トップページ修正　報告、お知らせを上に、コンセプトを下に

--- index.php
+++ index.php
@@ -20,42 +20,44 @@ require('./wp_log/wp-load.php');
 		<li><a href="./log/">Log</a></li>  
 		<li><a href="./volunteer/index.html">Volunteer</a></li>  
 	</ul> 
-  </header>
-  <div id="main">
-    <img src="./img/top.jpg" alt="top" width="950">
-	<div class="notice">
-		<h2>おしらせ</h2>
-		<?php
-		//paged = get_query_var( 'paged' );
-		//query_posts('cat=5&posts_per_page=5&paged='.$paged);
-		//if(have_posts()) : while ( have_posts() ) : the_post();
-		$args = array( 'numberposts' => 5, 'order'=> 'ASC', 'orderby' => 'title', 'cat' => '5' );
-		$postslist = get_posts( $args );
-		foreach ($postslist as $post) :  setup_postdata($post); ?> 
-		<ul>
-			<li><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a> -<span class="time"><?php the_time('Y年m月d日（D）'); ?></span></li>
+</header>
+<div id="main">
+	<div class="top-img">
+		<div class="notice">
+			<h2>おしらせ</h2>
+			<ul>
+			<?php
+			//paged = get_query_var( 'paged' );
+			//query_posts('cat=5&posts_per_page=5&paged='.$paged);
+			//if(have_posts()) : while ( have_posts() ) : the_post();
+			$args = array( 'numberposts' => 3, 'order'=> 'ASC', 'orderby' => 'title', 'cat' => '5' );
+			$postslist = get_posts( $args );
+			foreach ($postslist as $post) :  setup_postdata($post); ?> 
+				<li><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a> -<span class="time"><?php the_time('Y年m月d日（D）'); ?></span></li>
+			<?php
+			endforeach;
+			//endwhile;
+			//endif;
+			?>
+			</ul>
+		</div>
+		<div class="new">
+			<h2>最新報告</h2>
+			<ul>
+			<?php
+			$args = array( 'numberposts' => 5, 'order'=> 'DESC');
+			$postslist = get_posts( $args );
+			foreach ($postslist as $post) :  setup_postdata($post); ?> 
+				<li><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a> -<span class="time"><?php the_time('Y年m月d日（D）'); ?></span></li>
+			<?php
+			endforeach;
+			?>
 		</ul>
-		<?php
-		endforeach;
-		//endwhile;
-		//endif;
-		?>
-		<a href="http://rinshoundoshogai.sakura.ne.jp/log/?page_id=45">お知らせ一覧へ</a>
+		</div>
 	</div>
-	<div class="new">
-		<h2>最新報告</h2>
-		<?php
-		$args = array( 'numberposts' => 5, 'order'=> 'DESC');
-		$postslist = get_posts( $args );
-		foreach ($postslist as $post) :  setup_postdata($post); ?> 
-		<ul>
-			<li><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a> -<span class="time"><?php the_time('Y年m月d日（D）'); ?></span></li>
-		</ul>
-		<?php
-		endforeach;
-		?>
-
-	</div>
+	<p class="concept">
+	臨床運動障害研究会では研修会を通して、機能的・能力的アプローチを学び、ボランティアを通じ、日常生活の中で、必要とされている事はどのような事で私たちに求められていることは何か？今ある知識・創造ではなく、肌で感じる機会を提供できればと考えております。利益の一部を、紹介者外出サポート（ボランティア団体）、海外ボランティアへ寄付し、少しでも社会貢献に参加して参ります。
+	</p>
 <footer>
 <table border="0" width="950px" cellpadding="0">
 <tr>
